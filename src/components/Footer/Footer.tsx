2cf711cdbd064db7b8581a8649f39062d95755b0
박상훈 <bp4sp4@naver.com>
2026-04-14 14:53:45 +0900

    footer update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,15 +11,21 @@ export default function Footer() {
   const hasBottomBar = mounted && (pathname === '/' || pathname.startsWith('/program'))
   return (
     <footer className={styles.footer} data-bottom-bar={hasBottomBar ? 'true' : undefined}>
-      <p className={styles.company}>한평생유학</p>
+      <p className={styles.company}>한평생그룹</p>
       <p className={styles.info}>대표 양병웅 ｜ 사업자등록번호 227-88-03196</p>
       <p className={styles.info}>서울시 도봉구 창동 마들로13길 61 씨드큐브 905호</p>
       <p className={styles.info}>
         <a href="/terms" className={styles.link}>이용약관</a>
         {' ｜ '}
         <a href="/privacy" className={styles.link}>개인정보처리방침</a>
+        {' ｜ '}
+        <a
+          href="#"
+          className={styles.link}
+          onClick={(e) => { e.preventDefault(); window.open('http://www.ftc.go.kr/bizCommPop.do?wrkr_no=2278803196', 'bizCommPop', 'width=750,height=700'); }}
+        >사업자정보확인</a>
       </p>
-      <p className={styles.copyright}>2026 © 한평생유학. All rights reserved.</p>
+      <p className={styles.copyright}>2026 © 한평생그룹. All rights reserved.</p>
     </footer>
   )
 }
